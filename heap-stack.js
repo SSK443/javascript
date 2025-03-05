a9e5176a0c8f66c7677d750d21b840c3503591de
Suraj S Kumar <surajskumar443@gmail.com>
2025-03-05 13:40:13 +0530

    memory update

---
+++ heap-stack.js
@@ -0,0 +1,22 @@
+// stack memory all primitive data type.
+
+let app="ipay";
+let source=app;
+source='google';
+console.log(source);
+console.log(app);
+
+
+
+// heap memory non primitive data type.
+let emp={
+name:'sssk',
+age:33,
+gender:"male"
+}
+let suraj=emp;
+suraj=suraj.age=44;
+console.log(emp);
+console.log(suraj);
+
+
