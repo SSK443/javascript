bb56e19de1796b9fa3a6c7903c46724921bad0b0
Suraj S Kumar <surajskumar443@gmail.com>
2025-03-14 12:23:15 +0530

    iife

--- function.js
+++ function.js
@@ -1,83 +1,123 @@
-function greet(name){
-  return `Hello ${name}`
-}
-console.log(greet("Alice"));
+// function greet(name){
+//   return `Hello ${name}`
+// }
+// console.log(greet("Alice"));
 
-function add(a=10,b=20){
-  return a+b
-}
-console.log(add());
+// function add(a=10,b=20){
+//   return a+b
+// }
+// console.log(add());
 
-const multi=function(a=2,b=22){
-  return a*b
-}
-console.log(multi());
+// const multi=function(a=2,b=22){
+//   return a*b
+// }
+// console.log(multi());
 
-const mohan=function(){
-  console.log("hlo");
+// const mohan=function(){
+//   console.log("hlo");
   
-}
-mohan()
-const div=(a=44,b=4)=>{
-  return a/b
-}
-console.log(div());
+// }
+// mohan()
+// const div=(a=44,b=4)=>{
+//   return a/b
+// }
+// console.log(div());
 
-const saquare=x=>x*x;
-console.log(saquare(5));
+// const saquare=x=>x*x;
+// console.log(saquare(5));
 
-const adds=(a=3,b=4,c=5)=>a+b+c;
-console.log(adds());
+// const adds=(a=3,b=4,c=5)=>a+b+c;
+// console.log(adds());
 
-const news=()=>console.log('vffnvjfj');
-;
-console.log(news());
+// const news=()=>console.log('vffnvjfj');
+// ;
+// console.log(news());
 
-function add(num , num1){
- if(!num || num1){
-  console.log('value misssing');
-  return
- }
-return num*num1
+// function add(num , num1){
+//  if(!num || num1){
+//   console.log('value misssing');
+//   return
+//  }
+// return num*num1
  
   
+// }
+// const noan =add(4,4)
+// console.log(noan);
+
+
+// const passMultipleArugment=(...num1)=>{//use rest operator we can pass multiple arugments in single parameter return it in arrange
+//   return num1
+// }
+// console.log(passMultipleArugment(1,23,444,4444));
+
+// const handleObject={
+//   name:"dhumai",
+//   age:33,
+//   married:true
+// }
+// const handleObjectFunction=(object)=>{
+// return `His name is ${object.name} this age is ${object.age} and he is married ${object.married}`
+// }
+
+// console.log(handleObjectFunction(handleObject));
+
+// //or
+
+// console.log(handleObjectFunction({
+//   name:"milak singh",
+//   age:22,
+//   married:false
+// }));
+
+// const handleArray=[1,44,555,6666];
+
+// const handleArrayFunction=(array)=>{
+//   return ` this aray second item ${array[1]}]`
+// }
+// console.log(handleArrayFunction(handleArray));
+
+// //or
+
+// console.log(handleArrayFunction([1,34,55,66,6]));
+
+// const mowva={
+//   name:'arrowfunction',
+//   definition:'current context',
+//   currentContext:function(){
+// console.log(`to access key inside an object we use ${this.name} in current context object is block scope`);
+// console.log(this);
+
+
+//   }
+
+
+// }
+
+// mowva.currentContext()
+
+// mowva.name="scope"
+// mowva.currentContext()
+
+function nova(){
+  const namee="dffvr"
+  console.log(this.namee);
+  
 }
-const noan =add(4,4)
-console.log(noan);
+nova()
 
+// const nova2=()=>{
+//   console.log(this);
+  
+// }
+// nova2()
 
-const passMultipleArugment=(...num1)=>{//use rest operator we can pass multiple arugments in single parameter return it in arrange
-  return num1
+const add=(num,num1)=>{
+  return num+num1
 }
-console.log(passMultipleArugment(1,23,444,4444));
+console.log(add(1,3));
 
-const handleObject={
-  name:"dhumai",
-  age:33,
-  married:true
-}
-const handleObjectFunction=(object)=>{
-return `His name is ${object.name} this age is ${object.age} and he is married ${object.married}`
-}
+const muilt=()=>({name:"fvdfvdf"})
+console.log(muilt());
 
-console.log(handleObjectFunction(handleObject));
-
-//or
-
-console.log(handleObjectFunction({
-  name:"milak singh",
-  age:22,
-  married:false
-}));
-
-const handleArray=[1,44,555,6666];
-
-const handleArrayFunction=(array)=>{
-  return ` this aray second item ${array[1]}]`
-}
-console.log(handleArrayFunction(handleArray));
-
-//or
-
-console.log(handleArrayFunction([1,34,55,66,6]));
 
--- scope.js
+++ scope.js
@@ -1,24 +1,51 @@
-// {
-//   const ladoo=5;
-//   let jilebi=4;
+// // {
+// //   const ladoo=5;
+// //   let jilebi=4;
 
 
+// // }
+
+
+
+// // console.log(ladoo);
+// // console.log(jilebi);
+
+
+// function souse() {
+
+  
 // }
+// console.log(div);
+
+
+// let manmathan="fools";
+// manmathan=3;
+// console.log(manmathan);
+
+
+const one=()=>{
+  const onevar="goodbye";
+
+  function two(){
+const twovar="remesh";
+console.log(onevar);
 
 
 
-// console.log(ladoo);
-// console.log(jilebi);
+console.log(twovar);
 
+  }
+  two();
+}
 
-function souse() {
+one()
+
+if(true){
+  let user="assk"
+  if(user=="assk"){
+    console.log(user);
+    let ssk="wonder"
+  }
 
   
 }
-console.log(div);
-
-
-let manmathan="fools";
-manmathan=3;
-console.log(manmathan);
-
